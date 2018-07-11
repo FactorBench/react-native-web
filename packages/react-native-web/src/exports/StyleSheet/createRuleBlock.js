596c7d0337b2668ff94c4668d223c5111468c777
sertony <serkov_anton@mail.ru>
2018-07-11 22:07:08 +0300

    prepare package

--- packages/react-native-web/src/exports/StyleSheet/createRuleBlock.js
+++ packages/react-native-web/src/exports/StyleSheet/createRuleBlock.js
@@ -15,10 +15,11 @@ import prefixStyles from '../../modules/prefixStyles';
 const createDeclarationString = (prop, val) => {
   const name = hyphenateStyleName(prop);
   const value = normalizeValue(prop, val);
+  const flag = !name.startsWith('-') && !name.startsWith('pointer-') ? ' !important' : '';
   if (Array.isArray(val)) {
-    return val.map(v => `${name}:${v}`).join(';');
+    return val.map(v => `${name}:${v}${flag}`).join(';');
   }
-  return `${name}:${value}`;
+  return `${name}:${value}${flag}`;
 };
 
 /**
